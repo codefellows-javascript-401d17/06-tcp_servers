ba5ef4744c0ce77f889e6f76d23ea916ca334654
jamesbillard <jamesbillard12@hotmail.com>
2017-07-25 00:23:06 -0700

    added nickname feature

--- model/client.js
+++ model/client.js
@@ -3,6 +3,6 @@ const uuidv4 = require('uuid/v4');
 
 const Client = module.exports = function(socket){
   this.socket = socket;
-  this.nickname = `user_${Math.random()}`;
+  this.nickname = `user_${Math.round(Math.random() * 1000000)}`;
   this.id = uuidv4;
 };
--- server.js
+++ server.js
@@ -2,21 +2,67 @@
 
 const net = require('net');
 const EE = require('events');
+const colors = require('colors');
 const Client = require('./model/client.js');
 const PORT = process.env.PORT || 3000;
 const server = net.createServer();
 const ee = new EE();
 
-const pool = [];
 
-server.on('connection', function(socket) {
-  var client = new Client(socket);
-  console.log(client);
-  pool.push(client);
+
+const userPool = [];
+
+ee.on('@nickname', function(client, string){
+
+  if(string !== ''){
+    client.nickname = string.split(' ').shift().trim();
+    client.socket.write(`Your new nickname is ${client.nickname}.\n`);
+  }else {
+    client.socket.write(`${client.nickname}, you did not enter a nickname.\n`.red);
+  }
+});
+
+ee.on('@dm', function(client, string) {
+  let nickname = string.split(' ').shift().trim();
+  let message = string.split(' ').slice(1).join(' ').trim();
+
+  userPool.forEach( c => {
+    if (c.nickname === nickname) {
+      c.socket.write(`${client.nickname}: ${message}`);
+    }
+  });
+});
+
+ee.on('@all', function(client, string) {
+  userPool.forEach( c => {
+    c.socket.write(`${client.nickname}: ${string}`);
+  });
+});
+
+ee.on('default', function(client, string) {
+  client.socket.write('not a command \n');
 });
 
 
 
-server.listen(PORT, function(){
-  console.log('server online: ', PORT);
+
+
+server.on('connection', function(socket) {
+  var client = new Client(socket);
+  userPool.push(client);
+
+  socket.on('data', function(data) {
+    const command = data.toString().split(' ').shift().trim();
+
+    if (command.startsWith('@')) {
+      ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
+      return;
+    }
+
+    ee.emit('default', client, data.toString());
+  });
+});
+
+server.listen(PORT, function() {
+  console.log('server up:', PORT);
 });
