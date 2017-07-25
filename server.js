28858124bf1f3765019f0674bd50fa2c190aab63
Luis Manzo <manzosk3@gmail.com>
2017-07-24 21:39:37 -0700

    implements @all event

--- server.js
+++ server.js
@@ -9,6 +9,28 @@ const ee = new EE();
 
 var pool = [];
 
+ee.on('@all', function (client, string) {
+  pool.forEach(function (single_client) {
+    single_client.socket.write(`${single_client.nickname}: ${string}`);
+  })
+})
+
+server.on('connection', function (socket) {
+  var client = new Client(socket);
+  pool.push(client);
+
+
+  socket.on('data', function (data) {
+    const command = data.toString().split(' ').shift().trim();
+    if (command.startsWith('@')) {
+
+      ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
+      return;
+    }
+  })
+
+});
+
 server.listen(PORT, function () {
   console.log('server up on: ', PORT);
 });
