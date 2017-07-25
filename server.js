b635179994c0507e9e30d2c3fe6b97a35545962d
ohjonah <ohjonah@gmail.com>
2017-07-24 22:30:38 -0700

    Adds TCP Routing

--- server.js
+++ server.js
@@ -7,21 +7,72 @@ const PORT = process.env.PORT || 8000;
 const server = net.createServer();
 const ee = new EE();
 
-const pool = [];
+let pool = [];
+let helpGuide = [
+  '\n--------------------------------------------------\n',
+  '\n>>> cmd: \'@all <msg>\' to send a message to all\n',
+  '\n>>> cmd: \'@dm <name> <msg>\' to private message user\n',
+  '\n>>> cmd: \'@nickname <name>\' to change your name\n',
+  '\n>>> cmd: \'@exit\' to exit chat room\n',
+  '\n--------------------------------------------------\n'
+];
 
 ee.on('default', function(client) {
   client.socket.write('not a command \n');
 });
 
+ee.on('@help', function(client) {
+  helpGuide.forEach( helpCommand => {
+    client.socket.write(helpCommand);
+  });
+});
+
 ee.on('@all', function(client, string) {
   pool.forEach( c => {
-    c.socket.write(`${client.nickname}: ${string}`);
+    c.socket.write(`${client.nickname}: ${string} \n`);
   });
+});
+
+ee.on('@dm', function(client, string) {
+  let nickname = string.split(' ').shift().trim();
+  let message = string.split(' ').slice(1).join(' ').trim();
+
+  pool.forEach( c => {
+    if (c.nickname === nickname || c.id === nickname) {
+      c.socket.write(`DM from ${client.nickname}: ${message} \n`);
+    }
+  });
+});
+
+ee.on('@exit', function(client) {
+  let newPool = [];
+
+  pool.forEach( el => {
+    if ( client.id !== el.id) {
+      newPool.push(el);
+      el.socket.write(`${client.nickname} has left the chat! \n`);
+    }
+  });
+  pool = newPool;
+
+  client.socket.end();
+});
+
+ee.on('@nickname', function(client, string) {
+  client.nickname = string.split(' ').shift().trim();
 });
 
 server.on('connection', function(socket) {
   let client = new Client(socket);
+
+  pool.forEach( el => {
+    el.socket.write(`${client.nickname} has joined! \n`);
+  });
+
   pool.push(client);
+
+  socket.write('\nWelcome to Jonah\'s Chat Room \n');
+  socket.write('Type @help to see a list of available commands \n');
 
   socket.on('data', function(data) {
     const command = data.toString().split(' ').shift().trim();
@@ -35,6 +86,9 @@ server.on('connection', function(socket) {
   });
 });
 
+server.on('error', err => {
+  console.log(err);
+});
 
 server.listen(PORT, function() {
   console.log('Server Starting On:', PORT);
